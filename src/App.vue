3254d390a91a3c55fce96b6ebc032e6d7c3c4055
Ann-Jacqueline <annakaljob@gmail.com>
2024-05-01 22:26:11 +0200

    Mainpage V1

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,104 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <div id="app">
+      <main>
+        <div class="search-box">
+          <input type="text" class="search-bar" placeholder="Search..."/>
 
-    <div class="wrapper">
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+        </div>
+        <table class="table">
+          <thead>
+          <tr>
+            <th class="tableHeader"> Heute </th>
+            <th class="tableHeader">Morgen</th>
+            <th class="tableHeader">Favoriten</th>
+            <th class="tableHeader">Reisepläne</th>
+            <th class="tableHeader">Standorte Verwalten</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="(item, index) in items" :key="index">
+            <td>{{ item.col1 }}</td>
+            <td>{{ item.col2 }}</td>
+            <td>
+              <select v-model="selectedOptions[index]">
+                <option value="option1">Option 1</option>
+                <option value="option2">Option 2</option>
+                <option value="option3">Option 3</option>
+              </select>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </main>
     </div>
-  </header>
-
-  <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<script>
+export default {
+  name: 'app',
+  data(){
+    return{
+      api_key:'fb8defc9bea7f74a0f9993ac1624c942'
+
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  border: none;
+  outline: none;
 }
 
-.logo {
+body {
+  font-family: 'montserrat', sans-serif;
+  width: 100vw;
+  background-image: url('./assets/pic13.jpg');
+
+}
+
+#app {
+  transition: background-image 0.5s ease-in-out, opacity 0.5s ease-in-out;
+}
+
+main {
+  min-height: 100vh;
+  padding: 25px;
+
+}
+.search-box .search-bar{
+  margin-left: 350px;
   display: block;
-  margin: 0 auto 2rem;
+  width: 25%;
+  padding: 15px;
+  color: #1e1e1e;
+  font-size: 20px;
+  border-radius: 0px 16px 0px 16px;
+  box-shadow: 0px 0px 16px rgba(0,0,0,0.25) ;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: rgba(207, 196, 196, 0.42);
+  transition: 0.4s
 }
-
-nav {
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 8px rgba(0,0,0,0.25);
+  background-color: rgba(214, 219, 225, 0.75);
+  border-radius: 16px 0px 16px 0px;
+}
+.search-box {
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  margin-bottom: 30px;
+  margin-left:auto;
+
 }
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.tableHeader {
+  color: black;
+  padding-right: 200px;
+  font-size: large;
 }
 </style>
