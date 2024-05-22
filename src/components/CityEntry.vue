ff3b031c613903ee04633639171fc2ccc3e5f990
Ann-Jacqueline <annakaljob@gmail.com>
2024-05-22 14:42:03 +0200

    Auf Render Deployed aber noch keine Connection zum Backend

--- src/components/CityEntry.vue
+++ src/components/CityEntry.vue
@@ -23,6 +23,31 @@
 </template>
 
 <script>
+// export default {
+//   data() {
+//     return {
+//       cities: [
+//         { name: 'Berlin', latitude: 52.5162, longitude: 13.3777 },
+//         { name: 'Paris', latitude: 48.8566, longitude: 2.3522 },
+//         { name: 'Madrid', latitude: 40.4189, longitude: -3.6919 }
+//       ]
+//     };
+//   }
+
+import axios from 'axios'
+
+function createCity(): void {
+  const city = {
+    name: nameField.value,
+    latitude: latitudeField.value,
+    longitude: longitudeField.value
+  }
+
+  axios
+    .get<CityEntry>'http://localhost:8080/greeting'
+    .then((response) => CityEntry.value.push(response.data))
+    .catch(error)=> console.log(error))
+}
 
 </script>
 
